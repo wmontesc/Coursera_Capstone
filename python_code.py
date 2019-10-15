b1ed0f9682d5530728576a66590a0945e532523c
William Montes <montesito@Williams-MBP.home>
2019-10-15 17:59:23 +0200

    Adding the requested Python scrip for GitHub Project

--- python_code.py
+++ python_code.py
@@ -1,3 +1,10 @@
+### My first project to post on GitHub
+# This notebook is mainly for the Capstone Project
+# This is my first approach to GitHub
+
+# Importing Pandas and Numpy
 import pandas as pd
 import numpy as np
+
+# Printing "Hello Capstone Project Course!"
 print("Hello Capstone Project Course!")
